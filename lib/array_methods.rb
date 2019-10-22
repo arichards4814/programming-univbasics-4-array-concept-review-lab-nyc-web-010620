3fb4075ee8bc1a50af4a3c552c6823d23c12fab9
Andrew Richards <arichards4814@gmail.com>
2019-10-22 11:55:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   
   while array[count] do
     if array[count] == value_to_find
-      return array[count
+      return array[count]
     end
   count += 1
   end
